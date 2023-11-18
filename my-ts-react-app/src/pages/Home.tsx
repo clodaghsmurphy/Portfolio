fa236a86ce12a46ab28e4bb88ac5702bbe9a872b
clodaghsmurphy <clodagh9@yahoo.ie>
2023-11-18 19:48:28 +0100

    removed erros

--- my-ts-react-app/src/pages/Home.tsx
+++ my-ts-react-app/src/pages/Home.tsx
@@ -12,20 +12,20 @@ const TypeWriterConfigs = {
 }
 
 function Home() {
-    const { scrollYProgress } = useViewportScroll()
-    const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
+    // const { scrollYProgress } = useViewportScroll()
+    // const scale = useTransform(scrollYProgress, [0, 1], [0.2, 2]);
 
-    return (
-        <motion.div
-            style={{ scale }}
-        >
-            <motion.div
-                style={{
-                    scaleY: scrollYProgress
-                }}
-            />
-        </motion.div>
-    )
+    // return (
+    //     <motion.div
+    //         style={{ scale }}
+    //     >
+    //         <motion.div
+    //             style={{
+    //                 scaleY: scrollYProgress
+    //             }}
+    //         />
+    //     </motion.div>
+    // )
 	return (
     <div className="home-section">
         <div className="home-container">
