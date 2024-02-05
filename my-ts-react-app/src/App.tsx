c5df4bd90590ff9d1dca365cb11689ab500947f9
clodaghsmurphy <clodagh9@yahoo.ie>
2024-02-05 20:52:38 +0100

    added google tags

--- my-ts-react-app/src/App.tsx
+++ my-ts-react-app/src/App.tsx
@@ -16,12 +16,30 @@ import Banner from './pages/Banner';
 import Nav from './pages/Nav';
 import BlurRotate from './components/BlurRotate';
 import Projects from './pages/Projects';
+import Helmet from 'react-helmet';
 
 function App() {
    
 
   return (
     <div className="App">
+        <Helmet >
+            <script>
+                {`(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-52JL2F5M');`}
+                </script>
+              <noscript>
+                  <iframe
+                      src="https://www.googletagmanager.com/ns.html?id=GTM-52JL2F5M"
+                      height="0"
+                      width="0"
+                      style={{ display: 'none', visibility: 'hidden' }}
+                  />
+              </noscript>
+        </Helmet>
         < BlurRotate />
         <Header />
         <Banner />
